51889549c5b492b8d3794b0c47ff7154c653509c
yapinxxx <yapin.style@gmail.com>
2021-09-08 00:44:19 +0800

    update lib

--- leetcode useful function/lc_yp_lib.h
+++ leetcode useful function/lc_yp_lib.h
@@ -1,56 +1,42 @@
-/*need to set the size of nums array*/
-//nums = (int*)malloc(30000*sizeof(int)); //control the size of input array
-int leetcode_input(int **nums)//Read the input format like: [1,-8,5,......]
+int input(int **nums, int *target)
 {
     int i, numsSize;
-    char *ori_nums;
-    
-    ori_nums = (char*)malloc(2*sizeof(char));
+    char endchar;
     i = 0;
     numsSize = 0;
-    
-    while(1)
+    do
     {
-        scanf("%c",&ori_nums[i]);
-        if(ori_nums[i]=='['||ori_nums[i]==',')
+        scanf("%c",&endchar);
+        //printf("endchar %c numsSize %d\n",endchar, numsSize);
+        switch (endchar)
         {
-            ori_nums[i] = '\0';
-            i = 0;
-            continue;
-        }
-        else if(ori_nums[i]==']')
-        {
+        
+        case '[':
+            
+            scanf("%d",&(*nums)[numsSize]);
+            //printf("nums %d\n",(*nums)[numsSize]);
+            numsSize++;
+            break;
+        case ',':
+            
+            scanf("%d",&(*nums)[numsSize]);
+            //printf("nums %d\n",(*nums)[numsSize]);
+            numsSize++;
+            break;
+        
+        case'-':
+            scanf("%d",&(*nums)[numsSize]);
+            //printf("nums %d\n",(*nums)[numsSize]);
+            numsSize++;
+            break;
+        default:
+            //printf("Error\n");
             break;
         }
-        else if(ori_nums[i]=='-')
-        {
-            i++;
-            scanf("%c",&ori_nums[i]);
-            (*nums)[numsSize] = -1 * (ori_nums[i] - '0');
-            numsSize++;
-            i = 0;
-            continue;
-            
-        }
-        else
-        {
-            (*nums)[numsSize] = (ori_nums[i] - '0');
-            numsSize++;
-            i = 0;
-            continue;
-        }
 
-
-    }
+    }while(endchar!=']');
+    scanf("%d",target);
     
-    
-    //Double check the input whether true of false
-    // printf("%d\n",numsSize);
-    // for(i=0;i<numsSize;i++)
-    // {
-    //     printf("%d ",(*nums)[i]);
-    // }
-    // printf("\n");
-
     return numsSize;
+
 }
